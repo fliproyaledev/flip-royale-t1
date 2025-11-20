b9ebfb893a8970375dd44f5480bb6fc08de01cc2
fliproyaledev <eoscoder3@gmail.com>
2025-11-20 13:48:52 +0300

    Update settle-rounds.ts

--- pages/api/settle-rounds.ts
+++ pages/api/settle-rounds.ts
@@ -9,7 +9,7 @@ import {
   type RoundPick 
 } from '../../../lib/users'
 
-import { getPriceForToken } from '../../../lib/price'
+import { getPriceForToken } from '../../lib/users'
 
 // --- Utility: Duplicate Pick Nerf ---
 function nerfFactor(dup: number): number {
@@ -146,4 +146,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(500).json({ ok: false, error: err.message });
   }
 }
+
 
