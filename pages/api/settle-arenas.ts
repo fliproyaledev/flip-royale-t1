fb289db4910f3bf9b3a17d7f6dc43c3e4c91beca
fliproyaledev <eoscoder3@gmail.com>
2025-11-20 14:31:55 +0300

    Update settle-arenas.ts

--- pages/api/settle-arenas.ts
+++ pages/api/settle-arenas.ts
@@ -3,7 +3,7 @@ import {
   loadDuels,
   saveDuels,
   settleRoom,
-} from "../../../lib/duels";
+} from "../../lib/duels";
 
 function utcDayKey() {
   const d = new Date();
@@ -67,3 +67,4 @@ export default async function handler(
     return res.status(500).json({ ok: false, error: err.message });
   }
 }
+
